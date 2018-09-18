4a741a7b9353db2389176a68a5ac1e09a09b4e9c
Von Bonito Woo <bonitowoo@gmail.com>
2018-09-18 15:09:04 +0800

    Initial Commit

---
+++ DatingApp.API/Data/Datacontext.cs
@@ -0,0 +1,12 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class Datacontext : DbContext
+    {
+        public Datacontext(DbContextOptions<Datacontext> option) : base (option) {}
+
+        public DbSet<Value> Value { get; set; }
+    }
+}
