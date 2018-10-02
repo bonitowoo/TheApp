86c6467a4465c47dff76cb2956339522d1d0f220
bonito <bonitowoo@gmail.com>
2018-10-02 22:05:27 +0800

    second sprint

---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AuthService } from './_services/auth.service';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
+
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
+   ],
+   imports: [
+      FormsModule,
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [
+      AuthService
+   ],
+   bootstrap: [
+      AppComponent,
+      NavComponent
+   ]
+})
+export class AppModule { }
---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly Datacontext _context;
+        public AuthRepository(Datacontext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x=> x.Username == username);
+
+            if (user == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
+        }
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+               using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i <computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+            return true;
+        }
+
+        public async Task<User> RegisterAsync(User user, string password)
+        {
+            byte[] PasswordHash, PasswordSalt;
+            createPasswordHash(password, out PasswordHash, out PasswordSalt);
+
+            user.PasswordHash = PasswordHash;
+            user.PasswordSalt = PasswordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void createPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+        }
+
+        public async Task<bool> UserExist( string username)
+        {
+            if (await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+            
+            return false;
+        }
+    }
+}
